a6c5c6c56133776685fa548bde5c7cceec88c55d
Florian Landuyt <florian.landuyt.y2414@student.hogent.be>
2020-07-27 22:47:52 +0200

    Added home activity and added title fragment

--- Books/app/src/main/java/com/example/books/MainActivity.kt
+++ Books/app/src/main/java/com/example/books/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.books
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.books.databinding.ActivityHomeBinding
 
 class  MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = DataBindingUtil.setContentView<ActivityHomeBinding>(this, R.layout.activity_home)
     }
 }
