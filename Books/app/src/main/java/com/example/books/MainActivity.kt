a74daa28ea0f90893b1f41c44045d71bb312abc0
Florian Landuyt <florian.landuyt.y2414@student.hogent.be>
2020-08-04 14:20:41 +0200

    WIP: Added up button

--- Books/app/src/main/java/com/example/books/MainActivity.kt
+++ Books/app/src/main/java/com/example/books/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.books
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.books.R
 import com.example.books.databinding.ActivityMainBinding
 
@@ -14,5 +16,16 @@ class  MainActivity : AppCompatActivity() {
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this,
             R.layout.activity_main
         )
+
+        val navController = this.findNavController(R.id.myNavHostFragment)
+
+        NavigationUI.setupActionBarWithNavController(this, navController)
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return navController.navigateUp()
+    }
+
+
 }
