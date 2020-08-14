bcff54238803bc00309596230c8fc9cbdebe913c
Florian Landuyt <florian.landuyt.y2414@student.hogent.be>
2020-08-14 11:53:22 +0200

    Refractor added styles to list fragments

--- Books/app/src/androidTest/java/com/example/books/domain/AddHomoDeusToFavorites.kt
+++ Books/app/src/androidTest/java/com/example/books/domain/AddHomoDeusToFavorites.kt
@@ -2,12 +2,19 @@ package com.example.books.domain
 
 import android.view.View
 import android.view.ViewGroup
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.filters.LargeTest
 import androidx.test.internal.runner.junit4.AndroidJUnit4Builder
+import androidx.test.rule.ActivityTestRule
 import androidx.test.runner.AndroidJUnit4
 import com.example.books.MainActivity
+import com.example.books.R
 import org.hamcrest.Description
 import org.hamcrest.Matcher
+import org.hamcrest.Matchers
 import org.hamcrest.TypeSafeMatcher
 import org.junit.Rule
 import org.junit.Test
@@ -15,16 +22,31 @@ import org.junit.runner.RunWith
 
 @LargeTest
 @RunWith(AndroidJUnit4::class)
-class AddBookFavorite {
+class AddHomoDeusToFavorites {
 
-    @Rule
-//    var mActivityTestRule = ActivityTestRule(MainActivity::class.java)
+   @Rule
+    var mActivityTestRule = ActivityTestRule(MainActivity::class.java)
 
 
-    @Test
-    fun addBookFavorite() {
-        Thread.sleep(3000)
-    }
+//    @Test
+//    fun addBookFavorite() {
+//        Thread.sleep(3000)
+//
+//        val materialCardView = Espresso.onView(
+//            Matchers.allOf(
+//                withId(R.id.),
+//                childAtPosition(
+//                    childAtPosition(
+//                        withId(R.id.myNavHostFragment),
+//                        0
+//                    ),
+//                    0
+//                ),
+//                ViewMatchers.isDisplayed()
+//            )
+//        )
+//        materialCardView.perform(ViewActions.click())
+//    }
 
 
 
